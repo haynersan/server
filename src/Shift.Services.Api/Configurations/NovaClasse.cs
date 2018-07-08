d0b19118efdfa7b66f1c8f67bbc1ffdcac0a5c45
WELLINGTON HAYNER SANTOS SOUSA <whayners@hotmail.com>
2018-07-08 12:31:41 -0400

    Criei uma nova classe para Teste.....

---
+++ src/Shift.Services.Api/Configurations/NovaClasse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shift.Services.Api.Configurations
+{
+    public class NovaClasse
+    {
+        public string NovoTeste_GitHub { get; set; }
+    }
+
+}
